8d146d9ec3d85c6a2bba8724f4d596eb054853d6
Florian Cafiero <florian.cafiero@polytechnique.edu>
2026-01-08 15:30:56 +0100

    Update __init__.py

--- src/fabula/__init__.py
+++ src/fabula/__init__.py
@@ -1,1 +1,14 @@
+from .core import Fabula
+from .scorer import TransformersScorer
+from .segment import ParagraphSegmenter, RegexSentenceSegmenter, SlidingWindowTokenSegmenter
+from .schemas import ArcResult
+
+__all__ = [
+    "Fabula",
+    "TransformersScorer",
+    "ParagraphSegmenter",
+    "RegexSentenceSegmenter",
+    "SlidingWindowTokenSegmenter",
+    "ArcResult",
+]
 
